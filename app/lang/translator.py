57fcda5bd1d2e777e701aaa23a157f6ad6f0958c
OzgurAbd <OzgurAbd>
2024-07-19 23:45:21 +0300

    Translator Revised

--- app/lang/translator.py
+++ app/lang/translator.py
@@ -5,19 +5,19 @@ from typing import Any, Dict
 class Translator:
     _instances: Dict[str, 'Translator'] = {}
 
-    def __new__(cls, lang: str) -> 'Translator':
+    def __new__(cls, lang: str | None = "en") -> 'Translator':
         if lang not in cls._instances:
             cls._instances[lang] = super(Translator, cls).__new__(cls)
         return cls._instances[lang]
 
-    def __init__(self, lang: str = "en"):
+    def __init__(self, lang: str | None = "en"):
         self.lang = lang
 
     def t(self, key: str, **kwargs: Dict[str, Any]) -> str:
         file_key, *translation_keys = key.split('.')
 
         locale_module = importlib.import_module(
-            f'app.lang.{self.lang}.{file_key}')
+            f'lang.{self.lang}.{file_key}')
 
         translation = locale_module.locale
         for translation_key in translation_keys:
